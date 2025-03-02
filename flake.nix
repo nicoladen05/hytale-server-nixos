5069a20c2d98643cf387c48b27f75d5614272885
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-03-02 09:12:52 +0100

    sops + cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,16 @@
 {
   description = "Nixos config flake";
 
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -13,6 +23,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    sops-nix.url = "github:Mic92/sops-nix";
 
     stylix.url = "github:danth/stylix";
 
@@ -60,6 +72,7 @@
           ./hosts/desktop/configuration.nix
           inputs.nur.modules.nixos.default
           inputs.home-manager.nixosModules.home-manager
+          inputs.sops-nix.nixosModules.sops
           {
             home-manager.extraSpecialArgs = { inherit secrets; };
           }
@@ -75,9 +88,6 @@
           ./hosts/rpi5/configuration.nix
           inputs.raspberry-pi-nix.nixosModules.raspberry-pi
           inputs.raspberry-pi-nix.nixosModules.sd-image
-          inputs.stylix.nixosModules.stylix
-          inputs.home-manager.nixosModules.home-manager
-          inputs.nvf.nixosModules.default
         ];
       };
     };
