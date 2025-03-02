088d2cfab3ba3b84242839040909f49b45cfbaff
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-03-02 09:07:00 +0100

    sops secret management

---
+++ modules/nixos/system/sops.nix
@@ -0,0 +1,29 @@
+{ lib, config, ... }:
+
+{
+  options = {
+    system.sops = {
+      enable = lib.mkEnableOption "enable sops";
+
+      secretsFile = lib.mkOption {
+        type = lib.types.path;
+        default = ../../../secrets/secrets.yaml;
+      };
+
+      ageKeyFile = lib.mkOption {
+        type = lib.types.path;
+        default = builtins.toPath "/home/${config.system.userName}/.config/sops/age/keys.txt";
+      };
+    };
+  };
+
+  config = lib.mkIf config.system.sops.enable {
+    sops.defaultSopsFile = config.system.sops.secretsFile;
+    sops.defaultSopsFormat = "yaml";
+
+    sops.age.keyFile = config.system.sops.keyFile;
+
+    sops.secrets.password = { };
+    sops.secrets.password_strong = { };
+  };
+}
