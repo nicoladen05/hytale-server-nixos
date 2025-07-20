508f6bbebed7722bdac7baea2e79853e7ba0b666
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-07-20 15:26:52 +0000

    basic dev server setup through home-manager

--- hosts/dev-server/home.nix
+++ hosts/dev-server/home.nix
@@ -6,9 +6,20 @@
 }: {
   imports = [
     ../../modules/home-manager
+	../../modules/nixos/nvf.nix
   ];
 
   home-manager = {
     enable = true;
+
+	tmux.enable = true;
+	zsh.enable = true;
+  };
+
+  nvf.enable = true;
+
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
   };
 }
