35f25fbc24f6a9a95965f303f325735ddbe27158
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-08-02 13:46:17 +0200

    feat: enable ghostty

--- modules/home-manager/terminal/default.nix
+++ modules/home-manager/terminal/default.nix
@@ -16,7 +16,7 @@
 
   config = lib.mkIf config.home-manager.terminal.enable {
     home-manager.alacritty.enable = true;
-    home-manager.ghostty.enable = false;
+    home-manager.ghostty.enable = true;
     home-manager.nerd-fonts.enable = true;
     home-manager.zsh.enable = true;
     home-manager.tmux.enable = true;
