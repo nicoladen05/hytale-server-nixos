b843d410ba8cb32edb4c20f1756179b873a1440a
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-09-10 19:46:36 +0200

    feat: peristent key location

--- modules/nixos/system/sops.nix
+++ modules/nixos/system/sops.nix
@@ -12,7 +12,7 @@
 
       ageKeyFile = lib.mkOption {
         type = lib.types.path;
-        default = builtins.toPath "/home/${config.system.userName}/.config/sops/age/keys.txt";
+        default = builtins.toPath "/persistent/home/${config.system.userName}/.config/sops/age/keys.txt";
       };
     };
   };
