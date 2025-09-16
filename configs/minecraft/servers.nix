b3523299e1cd3a10a0b450c1abe2268fc5f2490c
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-09-16 18:56:19 +0200

    feat: template config files

---
+++ configs/minecraft/servers.nix
@@ -0,0 +1,23 @@
+{
+  luca = {
+    name = "Luca's Server";
+    version = "1.21.8";
+    type = "fabric";
+    host = "homeserver";
+    mods = {
+      enable = true;
+      packwizUrl = "";
+    };
+  };
+
+  create = {
+    name = "Create 1.21.1 Server";
+    version = "1.21.1";
+    type = "neoforge";
+    host = "vps";
+    mods = {
+      enable = true;
+      packwizUrl = "";
+    };
+  };
+}
