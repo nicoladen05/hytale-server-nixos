2946cd0b4928a140e399c545ab05f88c4b54a75d
Nicoladen <55351709+nicoladen05@users.noreply.github.com>
2025-09-17 09:35:19 +0200

    feat(homeserver): disko config

---
+++ hosts/homeserver/disko.nix
@@ -0,0 +1,49 @@
+{ lib, device, ... }:
+{
+    disko.devices = {
+        disk.disk1 = {
+            inherit device;
+            type = "disk";
+            content = {
+                type = "gpt";
+                partitions = {
+                    boot = {
+                        size = "512M";
+                        type = "EF00";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
+                        };
+                    };
+                    root = {
+                        size = "100%";
+                        content = {
+                            type = "btrfs";
+                            extraArgs = [ "-f" ]; # Override existing btrfs partitions
+                            subvolumes = {
+                                root = {
+                                    mountpoint = "/";
+                                    mountOptions = [ "noatime" "ssd" ];
+                                };
+                                nix = {
+                                    mountpoint = "/nix";
+                                    mountOptions = [ "compress=zstd" "noatime" "ssd" ];
+                                };
+                                data = {
+                                    mountpoint = "/data";
+                                    mountOptions = [ "compress=zstd" "ssd" ];
+                                };
+                                services = {
+                                    mountpoint = "/opt/services";
+                                    mountOptions = [ "compress=zstd" "ssd" ]
+                                }
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+}
