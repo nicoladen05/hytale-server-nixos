1897b8704997f6d449ffdadb2a20b2743b0c388d
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-09-17 20:40:55 +0200

    feat(sever): add trusted users

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -9,33 +9,34 @@ let
     hostName = "server";
 in
 {
-    imports = [
-        ./hardware-configuration.nix
-        ./disko.nix
+  imports = [
+      ./hardware-configuration.nix
+      ./disko.nix
 
-        ../../modules/nixos
-        ../../modules/homelab
-    ];
+      ../../modules/nixos
+      ../../modules/homelab
+  ];
 
-    # System configuration
-    system = {
-        enable = true;
-        sops.enable = true;
+  # System configuration
+  system = {
+      enable = true;
+      sops.enable = true;
 
-        # User account
-        inherit userName;
-        inherit hostName;
-        password = {
-            enable = true;
-            hashedPasswordFile = config.sops.secrets."user/nico/password_hash".path;
-        };
-        passwordlessRebuild = true;
-        ssh.enable = true;
-        tcpPorts = [ 22 ];
-    };
+      # User account
+      inherit userName;
+      inherit hostName;
+      password = {
+          enable = true;
+          hashedPasswordFile = config.sops.secrets."user/nico/password_hash".path;
+      };
+      passwordlessRebuild = true;
+      ssh.enable = true;
+      tcpPorts = [ 22 ];
+  };
 
   users.users.root.hashedPassword = "$6$FdDJt3LLc3Iu0r14$DKRv42b0IsqkW6OFkWr0WnUoxMPPaFUnSZgBFJKfR4elFeGRU3NfhP1rXbWd.b9073ZucRQrFto130F3eBVjj0";
   users.users.root.hashedPasswordFile = lib.mkForce null;
+  nix.settings.trusted-users = [ userName ];
 
-    system.stateVersion = "25.05";
+  system.stateVersion = "25.05";
 }
