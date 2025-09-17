e6ce5e1900cf7f9b1bd57504775a3ed709cb7344
Nicoladen <55351709+nicoladen05@users.noreply.github.com>
2025-09-17 09:49:51 +0200

    feat(homeserver): minimal configuration

---
+++ hosts/homeserver/configuration.nix
@@ -0,0 +1,37 @@
+{
+    config,
+    pkgs,
+    inputs,
+    ...
+}:
+
+let
+    userName = "nico";
+    hostName = "homeserver";
+in
+{
+    imports = [
+        ./hardware-configuration.nix
+        ./disko.nix
+
+        ../../modules/nixos
+        ../../modules/homelab
+    ];
+
+    # System configuration
+    system = {
+        enable = true;
+        sops.enable = true;
+
+        # User account
+        inherit userName;
+        inherit hostName;
+        password = {
+            enable = true;
+            hashedPasswordFile = config.sops.secrets."user/nico/password_hash".path;
+        };
+        passwordlessRebuild = true;
+    };
+
+    system.stateVersion = "25.05";
+}
