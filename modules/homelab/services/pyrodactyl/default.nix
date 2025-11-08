3ce1eb947fda89c7b7623a48ddb3899763ef3320
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-11-08 12:33:53 +0100

    fix: pyrodactyl

--- modules/homelab/services/pyrodactyl/default.nix
+++ modules/homelab/services/pyrodactyl/default.nix
@@ -1,8 +1,13 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 
-let 
+let
   cfg = config.homelab.services.pyrodactyl;
-in 
+in
 {
   options.homelab.services.pyrodactyl = {
     enable = lib.mkEnableOption "Pyrodactyl service";
@@ -24,94 +29,117 @@ in
     };
   };
 
-  config = 
-  let
-    dbName = "panel";
-    dbUser = "pyrodactyl";
-  in lib.mkIf cfg.enable {
-    systemd.tmpfiles.rules = [
-      "d ${cfg.path} 0775 pyrodactyl users -"
-      "d ${cfg.path}/database 0775 pyrodactyl users -"
-      "d ${cfg.path}/var 0775 pyrodactyl users -"
-      "d ${cfg.path}/nginx 0775 pyrodactyl users -"
-      "d ${cfg.path}/certs 0775 pyrodactyl users -"
-      "d ${cfg.path}/logs 0775 pyrodactyl users -"
-    ];
-
-    users.users.pyrodactyl = {
-      isSystemUser = true;
-      name = "pyrodactyl";
-      group = "users";
-    };
-
-    # Database
-    services.mysql = {
-      enable = true;
-      package = pkgs.mariadb;
-      ensureDatabases = [ dbName ];
-      ensureUsers = [
-        {
-          name = dbUser;
-          ensurePermissions = {
-            "${dbName}.*" = "ALL PRIVILEGES";
-          };
-        }
+  config =
+    let
+      dbName = "panel";
+      dbUser = "pyrodactyl";
+    in
+    lib.mkIf cfg.enable {
+      systemd.tmpfiles.rules = [
+        "d ${cfg.path} 0775 pyrodactyl users -"
+        "d ${cfg.path}/database 0775 pyrodactyl users -"
+        "d ${cfg.path}/var 0775 pyrodactyl users -"
+        "d ${cfg.path}/nginx 0775 pyrodactyl users -"
+        "d ${cfg.path}/certs 0775 pyrodactyl users -"
+        "d ${cfg.path}/logs 0775 pyrodactyl users -"
       ];
-      user = "pyrodactyl";
-    };
 
-    # Redis
-    services.redis = {
-      enable = true;
-    };
+      users.users.pyrodactyl = {
+        isSystemUser = true;
+        name = "pyrodactyl";
+        group = "users";
+      };
 
-    virtualisation.containers.enable = true;
-    virtualisation.oci-containers.backend = "podman";
-    virtualisation = {
-      podman = {
+      virtualisation.containers.enable = true;
+      virtualisation.oci-containers.backend = "podman";
+      virtualisation = {
+        podman = {
+          enable = true;
+          dockerCompat = true;
+          defaultNetwork.settings.dns_enabled = false;
+        };
+      };
+
+      # Database
+      services.mysql = {
         enable = true;
-        dockerCompat = true;
-        defaultNetwork.settings.dns_enabled = false;
+        package = pkgs.mariadb;
+        ensureDatabases = [ dbName ];
+        ensureUsers = [
+          {
+            name = dbUser;
+            ensurePermissions = {
+              "${dbName}.*" = "ALL PRIVILEGES";
+            };
+          }
+        ];
+        user = "pyrodactyl";
       };
-    };
 
-    # Panel
-    virtualisation.oci-containers.containers."pyrodactyl-panel" = {
-      image = "ghcr.io/pyrohost/pyrodactyl:latest";
-      autoStart = true;
-      pull = "newer";
-      ports = [ "${builtins.toString cfg.httpPort}:80" "${builtins.toString cfg.httpsPort}:443" ];
-      environment = {
-        APP_URL = "https://${cfg.url}";
-        APP_SERVICE_AUTHOR = "noreply@${cfg.url}"; 
-        APP_ENV = "production";
-        APP_ENVIRONMENT_ONLY = "false";
-        CACHE_DRIVER = "redis";
-        SESSION_DRIVER = "redis";
-        QUEUE_DRIVER = "redis";
-        REDIS_HOST = "127.0.0.1";
-        DB_CONNECTION = "mysql";
-        DB_HOST = "127.0.0.1";
-        DB_PORT = "3306";
-        DB_DATABASE = "panel";
-        DB_USERNAME = "pyrodactyl";
-        DB_PASSWORD = "changeme";
-        HASHIDS_LENGTH = "8";
+      # Redis
+      services.redis.servers.pyrodactyl = {
+        enable = true;
+        port = 6380;
+        bind = "0.0.0.0";
       };
-      volumes = [
-        "${cfg.path}/var:/app/var/"
-        "${cfg.path}/nginx:/etc/nginx/http.d/"
-        "${cfg.path}/certs:/etc/letsencrypt/"
-        "${cfg.path}/logs:/app/storage/logs/"
+
+      # Panel
+      virtualisation.oci-containers.containers."pyrodactyl-panel" = {
+        image = "ghcr.io/pyrohost/pyrodactyl:latest";
+        autoStart = true;
+        pull = "newer";
+        ports = [
+          "${builtins.toString cfg.httpPort}:80"
+          "${builtins.toString cfg.httpsPort}:443"
+        ];
+        environment = {
+          APP_URL = "https://${cfg.url}";
+          APP_SERVICE_AUTHOR = "noreply@${cfg.url}";
+          APP_ENV = "production";
+          APP_KEY = "base64:g617PTArwVQJ4T5NBA3GrI8xb65kUcb1I0AbHagYP0k=";
+          APP_ENVIRONMENT_ONLY = "false";
+          CACHE_DRIVER = "redis";
+          SESSION_DRIVER = "redis";
+          QUEUE_DRIVER = "redis";
+          REDIS_HOST = "host.docker.internal";
+          REDIS_PORT = "6380";
+          DB_CONNECTION = "mysql";
+          DB_HOST = "host.docker.internal";
+          DB_PORT = "3306";
+          DB_DATABASE = "panel";
+          DB_USERNAME = "pyrodactyl";
+          DB_PASSWORD = "changeme";
+          HASHIDS_LENGTH = "8";
+        };
+        volumes = [
+          "${cfg.path}/var:/app/var/"
+          "${cfg.path}/nginx:/etc/nginx/http.d/"
+          "${cfg.path}/certs:/etc/letsencrypt/"
+          "${cfg.path}/logs:/app/storage/logs/"
+        ];
+        extraOptions = [
+          "--add-host=host.docker.internal:host-gateway"
+        ];
+      };
+
+      networking.firewall.allowedTCPPorts = [
+        8443
+        3306
+        6380
       ];
-    };
 
-    networking.firewall.allowedTCPPorts = [ 8443 ];
-
-    services.caddy.virtualHosts."${cfg.url}" = {
-      extraConfig = ''
-        reverse_proxy 127.0.0.1:${builtins.toString cfg.httpPort}
-      '';
+      services.caddy.virtualHosts."${cfg.url}" = {
+        extraConfig = ''
+          
+                    
+                              
+                                        
+                                                  
+                                                            
+                                                                      
+                                                                                
+                                                                                          reverse_proxy 127.0.0.1:${builtins.toString cfg.httpPort}
+        '';
+      };
     };
-  };
 }
