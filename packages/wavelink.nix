cd441501f4dfb5a79c46ac7e91380bd051d4eeb7
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-11-24 09:49:43 +0100

    fix: remove async-timeout dependency for wavelink

--- packages/wavelink.nix
+++ packages/wavelink.nix
@@ -8,30 +8,32 @@ let
   inherit (python313Packages) buildPythonPackage;
 in
 buildPythonPackage rec {
-    pname = "wavelink";
-    version = "3.4.1";
-    pyproject = true;
+  pname = "wavelink";
+  version = "3.4.1";
+  pyproject = true;
 
-    src = fetchPypi {
-      inherit pname version;
-      hash = "sha256-CqLbXk59awcCpp5M23FhtEe3pXNZVbzlu62ygjXFR44=";
-    };
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-CqLbXk59awcCpp5M23FhtEe3pXNZVbzlu62ygjXFR44=";
+  };
 
-    build-system = with python313Packages; [
-      setuptools
-    ];
-    
-    dependencies = with python313Packages; [
-      aiohttp
-      discordpy
-      yarl
-      async-timeout
-      typing-extensions
-    ];
+  build-system = with python313Packages; [
+    setuptools
+  ];
 
-    # Patch requirements.txt to replace discord.py with py-cord
-    postPatch = ''
-      substituteInPlace requirements.txt \
-        --replace "discord.py" "py-cord"
-    '';
+  dependencies = with python313Packages; [
+    aiohttp
+    discordpy
+    yarl
+    typing-extensions
+  ];
+
+  # Patch requirements.txt to replace discord.py with py-cord
+  postPatch = ''
+    substituteInPlace requirements.txt \
+      --replace "discord.py" "py-cord"
+
+    substituteInPlace requirements.txt \
+      --replace "async_timeout" ""
+  '';
 }
