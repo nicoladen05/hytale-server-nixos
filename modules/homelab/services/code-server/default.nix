91ac36ba7c2f70348526c8dd1a93297d53715aee
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-12-27 20:49:23 +0100

    fix: add tkinter

--- modules/homelab/services/code-server/default.nix
+++ modules/homelab/services/code-server/default.nix
@@ -27,12 +27,12 @@ in
       port = 8443;
     };
 
-    users.users.code-server.packages = [ 
-      pkgs.python313.withPackages [
+    users.users.code-server.packages = [
+      (pkgs.python313.withPackages [
         pkgs.python313Packages.tkinter
-      ]
+      ])
       pkgs.xvfb-run
-      pkgs.uv 
+      pkgs.uv
     ];
 
     networking.firewall.allowedTCPPorts = [ 8443 ];
