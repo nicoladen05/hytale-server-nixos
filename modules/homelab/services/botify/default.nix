b41a68e1f80ab0210f469de69b016b489301f489
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-01-17 23:17:15 +0100

    refactor: use python overrides for botify

--- modules/homelab/services/botify/default.nix
+++ modules/homelab/services/botify/default.nix
@@ -8,39 +8,34 @@
 let
   cfg = config.homelab.services.botify;
 
-  # botifyPkgs = import <nixpkgs> {
-  #   overlays = [ (import ../../../../overlays/default.nix) ];
-  # };
-
-  pycordPkg = pkgs.python313Packages.pycord.override { voiceSupport = true; };
-
-  pythonEnvironment = pkgs.python313.withPackages (
-    p: with p; [
-      pycordPkg
-      python-dotenv
-      pexpect
-      (aiohttp.overridePythonAttrs (old: {
+  python = pkgs.python313.override {
+    packageOverrides = self: super: {
+      aiohttp = super.aiohttp.overridePythonAttrs (old: {
         dependencies = builtins.filter (x: x.pname != "aiodns") old.dependencies;
         doCheck = false;
-      }))
+      });
+    };
+  };
+
+  pythonEnvironment = python.withPackages (
+    p: with p; [
+      discordpy
+      python-dotenv
       mcstatus
       requests
-      # We need to replace discord.py with pycord in the wavelink
-      # build inputs, otherwise we'll have two derivations providing
-      # the discord namespace
-      (wavelink.overridePythonAttrs (old: {
-        dependencies =
-          (builtins.filter (pkg: pkg.pname != "discord.py") (old.propagatedBuildInputs or [ ]))
-          ++ [ pycordPkg ];
-      }))
+      rich
+      yt-dlp
     ]
   );
 
   botify = pkgs.fetchFromGitHub {
     owner = "nicoladen05";
     repo = "botify";
-    rev = "6332a9754927fef988ae14ba1b07279eb7de5049";
-    sha256 = "sha256-8fNZImDdZ+DmoJvmhDYtIowu6x79bGgtGhUVNY4hl8A=";
+    rev = "5577ca64981ca76184ef0d50e9237993fa5e0954";
+    hash = "sha256-d/xl+QbNYwPfgW8AQH5hExycglmjtCDwqNr71qLwWW0=";
+    postFetch = ''
+      rm -rf $out/.direnv
+    '';
   };
 in
 {
@@ -58,11 +53,11 @@ in
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
-        ExecStart = "${pythonEnvironment}/bin/python ${botify}/src/bot.py";
+        WorkingDirectory = "${botify}";
+        Environment = "PYTHONPATH=${botify},PATH=${pkgs.ffmpeg}/bin:/run/current-system/sw/bin:/bin:/usr/bin";
+        ExecStart = "${pythonEnvironment}/bin/python -m src.bot";
         Restart = "always";
         EnvironmentFile = cfg.tokenFile;
-        # Required for minecraft server commands
-        Environment = "PATH=${pkgs.openssh}/bin:/run/current-system/sw/bin:/bin:/usr/bin";
         User = "${config.system.userName}";
         Group = "users";
       };
